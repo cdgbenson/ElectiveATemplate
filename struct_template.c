c5e1c843beebd4c9c923d13add1ef06ee4948c41
cdgbenson <62872466+cdgbenson@users.noreply.github.com>
2020-03-30 15:41:14 +0800

    Template for the Task on Struct

---
+++ struct_template.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+/*Declare a struct named Point here*/
+
+/*Declare the functions here*/
+
+int main()
+{
+    Point test1, test2, test3; //Point is not declared yet, try to look up how it should be created.
+    printf("Enter the cooridnate of Point 1: ");
+    scanf("%f %f", &test1.x, &test1.y);
+    /*Resume your code here*/
+    
+    
+    
+    return 0;
+}
+
